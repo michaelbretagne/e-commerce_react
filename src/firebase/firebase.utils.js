b2db139e6cf820a92ab4c3cb5eaf9bbf89cbe86a
Michael <michael.bretagne@gmail.com>
2019-08-05 09:31:19 -0700

    Integrate Firebase and add signin/signout feature

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+	apiKey: "AIzaSyB08JctKfvzn8A5NKpV3PrPRSy9AgbZpGc",
+	authDomain: "e-commerce-d2c15.firebaseapp.com",
+	databaseURL: "https://e-commerce-d2c15.firebaseio.com",
+	projectId: "e-commerce-d2c15",
+	storageBucket: "",
+	messagingSenderId: "28938703381",
+	appId: "1:28938703381:web:c76f102735b0d850",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
