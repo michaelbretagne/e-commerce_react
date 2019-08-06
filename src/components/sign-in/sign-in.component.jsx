3d9268ca6447140e5d2c73320066e32f806b1258
Michael <michael.bretagne@gmail.com>
2019-08-06 11:27:23 -0700

    Add user authentication using firebase

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 import "./sign-in.styles.scss";
 
 class SignIn extends React.Component {
@@ -14,9 +14,15 @@ class SignIn extends React.Component {
 		};
 	}
 
-	handleSubmit = event => {
+	handleSubmit = async event => {
 		event.preventDefault();
-		this.setState({ email: "", password: "" });
+		const { email, password } = this.state;
+		try {
+			await auth.signInWithEmailAndPassword(email, password);
+			this.setState({ email: "", password: "" });
+		} catch (error) {
+			console.log(error);
+		}
 	};
 
 	handleChange = event => {
@@ -36,7 +42,7 @@ class SignIn extends React.Component {
 						type="email"
 						handleChange={this.handleChange}
 						value={this.state.email}
-						label="email"
+						label="Email"
 						required
 					/>
 					<FormInput
@@ -44,7 +50,7 @@ class SignIn extends React.Component {
 						type="password"
 						value={this.state.password}
 						handleChange={this.handleChange}
-						label="password"
+						label="Password"
 						required
 					/>
 					<div className="buttons">
